238cb46826622b77c7b8ff58a460b4cdb11dec4b
Lucas Barbier <lucasbarbr@outlook.com>
2025-09-30 14:21:14 +0200

    Add a pre-commit configuration file and clang-format file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
 #include <array>
+#include <iostream>
 
-#include "SimInfo.h"
-#include "Init.h"
 #include "ComputeDt.h"
 #include "IOManager.h"
+#include "Init.h"
+#include "SimInfo.h"
 #include "Update.h"
 
 using namespace fv2d;
@@ -28,15 +28,14 @@ int main(int argc, char **argv) {
     auto device_params = params.device_params;
 
     // Allocating main views
-    Array U    = Kokkos::View<real_t***>("U", device_params.Nty, device_params.Ntx, Nfields);
-    Array Q    = Kokkos::View<real_t***>("Q", device_params.Nty, device_params.Ntx, Nfields);
-
+    Array U = Kokkos::View<real_t ***>("U", device_params.Nty, device_params.Ntx, Nfields);
+    Array Q = Kokkos::View<real_t ***>("Q", device_params.Nty, device_params.Ntx, Nfields);
 
     // Misc vars for iteration
     real_t t = 0.0;
     int ite = 0;
     real_t next_save = 0.0;
-    
+
     // Initializing primitive variables
     InitFunctor init(params);
     UpdateFunctor update(params);
@@ -61,7 +60,7 @@ int main(int argc, char **argv) {
     while (t + device_params.epsilon < params.tend) {
       bool save_needed = (t + device_params.epsilon > next_save);
 
-      dt = computeDt.computeDt(Q, (ite == 0 ? params.save_freq : next_save-t), t, next_log == 0);
+      dt = computeDt.computeDt(Q, (ite == 0 ? params.save_freq : next_save - t), t, next_log == 0);
       if (next_log == 0)
         next_log = params.log_frequency;
       else
